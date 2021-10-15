c7b51147ca8fcc390a66c710a921d17db052688c
Bas van Beek <b.f.van.beek@vu.nl>
2021-10-15 13:43:30 +0200

    MAINT: Remove a, now unsupported, Seaborn option

--- sbu/data/__init__.py
+++ sbu/data/__init__.py
@@ -5,10 +5,7 @@ import yaml
 
 _filename = join(dirname(__file__), 'palette.yaml')
 with open(_filename, 'r') as f:
-    lineplot_dict = yaml.load(f, Loader=yaml.Loader)
-    try:
-        style_overide = lineplot_dict.pop('style_overide')
-    except KeyError:
-        style_overide = {}
+    lineplot_dict = yaml.load(f, Loader=yaml.SafeLoader)
+    style_overide = lineplot_dict.pop('style_overide', {})
 
 __all__ = ['lineplot_dict', 'style_overide']
