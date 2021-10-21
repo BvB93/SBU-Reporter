0a1b50afecf269b20046b95e12320d282ef78dbe
Bas van Beek <b.f.van.beek@vu.nl>
2021-10-21 14:35:40 +0200

    Update parse_yaml.py

--- sbu/parse_yaml.py
+++ sbu/parse_yaml.py
@@ -132,8 +132,7 @@ def validate_usernames(df: pd.DataFrame) -> None:
 
     """
     _usage = check_output(['accinfo']).decode('utf-8')
-    iterator = list(i.rstrip("\n") for i in _usage.splitlines())
-    import pdb; pdb.set_trace()
+    iterator = iter(_usage.splitlines())
     for i in iterator:
         if i == "# Users linked to this account":
             next(iterator)
